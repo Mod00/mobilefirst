0490d46d890de77246e8ddcd38ddad7653bfc6d1
pathomporn <s6202041620061@email.kmutnb.ac.th>
2021-08-03 17:07:47 +0700

    เพิ่ม function

--- dart/helloworld.dart
+++ dart/helloworld.dart
@@ -2,13 +2,11 @@ void main(){
  callWeb("Google",port: "443", protocal: "https");
 }
 
-
 void callWeb(String web, {String protocal ="http" ,String port ="80"}) => print("$web $protocal $port"); 
-
 
 String callGen(int year){
     print(year);
-  var x =  (year >= 1996) ? print("Gne Z"):print("Gen Y หรือ Upper");
+  var x =  (year >= 1996) ? "Gne Z":"Gen Y หรือ Upper";
   return x;
 }
 int callColor([String newcolor = "pink"]){
