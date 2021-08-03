b8999ecadef26b8e1be366c424d989dd67f63310
pathomporn <s6202041620061@email.kmutnb.ac.th>
2021-08-03 17:05:36 +0700

    เพิ่ม function.dart

--- dart/helloworld.dart
+++ dart/helloworld.dart
@@ -1,6 +1,23 @@
 void main(){
-    print("Pathomporn");
-    print("Suwannaphak");
-    print("21 year old");
-    print("Mod")
+ callWeb("Google",port: "443", protocal: "https");
+}
+
+
+void callWeb(String web, {String protocal ="http" ,String port ="80"}) => print("$web $protocal $port"); 
+
+
+String callGen(int year){
+    print(year);
+  var x =  (year >= 1996) ? print("Gne Z"):print("Gen Y หรือ Upper");
+  return x;
+}
+int callColor([String newcolor = "pink"]){
+    List<dynamic> colors = ["red","blue","green","black"];
+
+    colors.add(newcolor);
+
+    for(var i in colors){
+        print(i);
+    }
+    return colors.length;
 }
